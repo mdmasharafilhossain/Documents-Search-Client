e3809cea8cd4d2a82245b46df7cfdbf66fdb6c17
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-11-05 13:07:42 +0600

    Bug Fixed

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 const Banner: React.FC = () => {
     return (
         <div>
@@ -7,16 +9,20 @@ const Banner: React.FC = () => {
                     <div className="hero-overlay bg-opacity-60"></div>
                     <div className="hero-content text-center text-neutral-content">
                         <div className="">
-                            <h1 className="mb-5 text-6xl font-bold text-center">Welcome to <span className="text-orange-400">DocFind</span> </h1>
+                            <h1 className="mb-5 text-3xl md:text-5xl lg:text-6xl font-bold text-center">Welcome to <span className="text-orange-400">DocFind</span> </h1>
+
+                            {/* This sentence is Ai generated */}
                             <p className="max-w-3xl text-base sm:text-lg md:text-xl lg:text-2xl font-medium text-gray-200 leading-relaxed">
                                 Empower your legal research with <span className="text-orange-400 font-semibold">DocFind</span> your
                                 intelligent companion for searching, analyzing, and summarizing complex legal documents. Whether
                                 you’re a student, lawyer, or researcher, DocFind helps you find the right information faster,
                                 smarter, and easier.
                             </p>
+                             <Link to="/search-docs">
                              <button className="mt-6 px-6 py-3 bg-orange-400 text-white font-bold rounded-md hover:bg-orange-500 transition">
                               Start Searching
                              </button>
+                             </Link>
 
                         </div>
                     </div>
